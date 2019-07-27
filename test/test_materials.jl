5a168722ad2c6347653ae8555f6f038099487c1a
Petr Krysl <pkrysl@ucsd.edu>
2019-07-27 16:00:21 +0200

    Add tests for St Venant-Kirchhoff

--- test/test_materials.jl
+++ test/test_materials.jl
@@ -1,6 +1,4 @@
-"""
-Test of the neohookean material model
-"""
+
 module m1test1
 using FinEtools
 using FinEtoolsDeforLinear.DeforModelRedModule: DeforModelRed3D
@@ -50,3 +48,155 @@ end
 end
 using .m1test1
 m1test1.test()
+
+module m2test1
+using FinEtools
+using FinEtoolsDeforLinear.DeforModelRedModule: DeforModelRed3D
+using FinEtoolsDeforNonlinear
+using LinearAlgebra: norm
+using Test
+function test()
+    mr = DeforModelRed3D
+    E, nu = 7.0*phun("MPa"), 0.3
+
+    m = FinEtoolsDeforNonlinear.MatDeforStVKModule.MatDeforStVK(mr, E, nu)
+    # @show m
+    update! = FinEtoolsDeforNonlinear.MatDeforNonlinearModule.update!
+
+    stress = fill(0.0, 6)
+    output = FFlt[]
+    statev = FFlt[]
+    tn = 0.0
+    dtn = 0.0
+    loc = [0.0 0.0 0.0]
+    label = 0
+    quantity=:nothing
+
+    Fn1 = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress) == 0.0
+
+    Fn1 = [1.01 0 0; 0 1.0 0; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress-[95648.94230769236, 40184.69154607771, 40184.69154607771, 0.0, 0.0, 0.0]) / E < 1.0e-7
+
+    Fn1 = [1.0 0 0; 0 1.01 0; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress-[40184.6915460777, 95648.94230769236, 40184.69154607771, 0.0, 0.0, 0.0] ) / E < 1.0e-7
+
+    Fn1 = [1.0 0 0; 0 1.0 0.01; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress-[201.92307692305477, 740.4317307692086, 471.1538461537944, 0.0, 0.0, 26927.78846153846]) / E < 1.0e-7
+end
+end
+using .m2test1
+m2test1.test()
+
+module m1test2
+using FinEtools
+using FinEtoolsDeforLinear.DeforModelRedModule: DeforModelRed3D
+using FinEtoolsDeforNonlinear
+using LinearAlgebra: norm
+using Test
+function test()
+    mr = DeforModelRed3D
+    E, nu = 7.0*phun("MPa"), 0.3
+    m = FinEtoolsDeforNonlinear.MatDeforNeohookeanModule.MatDeforNeohookean(mr, E, nu)
+    # @show m
+    update! = FinEtoolsDeforNonlinear.MatDeforNonlinearModule.update!
+
+    stress = fill(0.0, 6)
+    output = FFlt[]
+    statev = FFlt[]
+    tn = 0.0
+    dtn = 0.0
+    loc = [0.0 0.0 0.0]
+    label = 0
+    quantity=:nothing
+
+    Fn1 = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress) == 0.0
+
+    Fn1 = [1.01 0 0; 0 1.0 0; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress-[93365.8, 39786.2, 39786.2, 0.0, 0.0, 0.0]) / E < 1.0e-7
+
+    Fn1 = [1.0 0 0; 0 1.01 0; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress-[39786.2, 93365.8, 39786.2, 0.0, 0.0, 0.0]) / E < 1.0e-7
+
+    Fn1 = [1.0 0 0; 0 1.0 0.00001; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress-[0.0, 0.0002692307915070229, 0.0, 0.0, 0.0, 26.923076923076923]) / E < 1.0e-7
+end
+end
+using .m1test2
+m1test2.test()
+
+module m2test2
+using FinEtools
+using FinEtoolsDeforLinear.DeforModelRedModule: DeforModelRed3D
+using FinEtoolsDeforNonlinear
+using LinearAlgebra: norm
+using Test
+function test()
+    mr = DeforModelRed3D
+    E, nu = 7.0*phun("MPa"), 0.3
+    
+    m = FinEtoolsDeforNonlinear.MatDeforStVKModule.MatDeforStVK(mr, E, nu)
+    # @show m
+    update! = FinEtoolsDeforNonlinear.MatDeforNonlinearModule.update!
+
+    stress = fill(0.0, 6)
+    output = FFlt[]
+    statev = FFlt[]
+    tn = 0.0
+    dtn = 0.0
+    loc = [0.0 0.0 0.0]
+    label = 0
+    quantity=:nothing
+
+    Fn1 = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress) == 0.0
+
+    Fn1 = [1.01 0 0; 0 1.0 0; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress-[95648.94230769236, 40184.69154607771, 40184.69154607771, 0.0, 0.0, 0.0]) / E < 1.0e-7
+
+    Fn1 = [1.0 0 0; 0 1.01 0; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress-[40184.6915460777, 95648.94230769236, 40184.69154607771, 0.0, 0.0, 0.0] ) / E < 1.0e-7
+
+    Fn1 = [1.0 0 0; 0 1.0 0.00001; 0 0 1.0]
+    Fn = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+    update!(m, statev, stress, output, Fn1, Fn, tn, dtn, loc, label, quantity)
+    # @show stress
+    @test norm(stress-[0.0002019230936302673, 0.0007403846321389213, 0.0004711538851372903, 0.0, 0.0, 26.92307692778846]) / E < 1.0e-7
+end
+end
+using .m2test2
+m2test2.test()
