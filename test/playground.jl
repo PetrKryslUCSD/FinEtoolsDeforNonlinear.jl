0959937dcaa27be17a61330a3994c648c052c53b
Petr Krysl <pkrysl@ucsd.edu>
2019-07-29 21:07:42 +0200

    preliminaries

--- test/playground.jl
+++ test/playground.jl
@@ -1,74 +1,32 @@
-module mneohook1
+module testelasticitytensor1
 using FinEtools
-using FinEtoolsDeforNonlinear
-using FinEtoolsDeforNonlinear.MatDeforNeohookeanModule: MatDeforNeohookean
-using FinEtoolsDeforNonlinear.FEMMDeforNonlinearModule: FEMMDeforNonlinear
-using FinEtoolsDeforNonlinear.AlgoDeforNonlinearModule: nonlinearstatics
-using LinearAlgebra: norm
-using Statistics: mean
-using SparseArrays
-using DelimitedFiles
-using Interpolations
-using UnicodePlots
+using FinEtoolsDeforNonlinear.MatDeforNonlinearModule: totalLagrangean2current!
+using LinearAlgebra
 using Test
+
 function test()
-    mr = DeforModelRed3D
-    E, nu = 7.0*phun("MPa"), 0.3
-    m = MatDeforNeohookean(mr, E, nu)
-    L= 6/2*phun("mm");
-    H = 2/2*phun("mm");
-    W = 2/2*phun("mm");
-    umag = 1.5*phun("mm");# Magnitude of the displacement
-    nincr = 48
-    utol = 10e-7;
-    graphics = ~true;
-    maxdu_tol = W/1e7;
-    maxiter = 5
-    tolerance = W / 1000
-
-    fens, fes = H8block(L, W, H, 2, 1, 1)
-
-    l1  = selectnode(fens; box = [0,0,-Inf,Inf,-Inf,Inf], inflate  =  tolerance)
-    e1 = FDataDict("node_list"=>l1, "component"=>1, "displacement"=>0.0)
-    l2  = selectnode(fens; box = [-Inf,Inf,0,0,-Inf,Inf], inflate  =  tolerance)
-    e2 = FDataDict("node_list"=>l2, "component"=>2, "displacement"=>0.0)
-    l3  = selectnode(fens; box = [-Inf,Inf,-Inf,Inf,0,0], inflate  =  tolerance)
-    e3 = FDataDict("node_list"=>l3, "component"=>3, "displacement"=>0.0)
-
-    movel1  = selectnode(fens; box = [L,L,-Inf,Inf,-Inf,Inf], inflate  =  tolerance)
-    table = LinearInterpolation([0, 0.25, 0.5, 0.75,1], umag*[0,-1,0,1,0])
-    move(lambda) = table(lambda);
-    e4 = FDataDict("node_list"=>movel1, "component"=>1, "displacement"=>move)
-
-    # Rmout = fill(0.0, 3, 3)
-    # rv = vec([-0.56 -0.1361 0.35])
-    # rotmat3!(Rmout, rv)
-    # mcsys = CSys(Rmout)
-    # femm = FEMMDeforNonlinear(mr, IntegDomain(fes, GaussRule(3, 2)), mcsys, m)
-    femm = FEMMDeforNonlinear(mr, IntegDomain(fes, GaussRule(3, 2)), m)
-
-    region1 = FDataDict("femm"=>femm)
-    modeldata =  FDataDict("fens"=> fens, "regions"=>  [region1],  "essential_bcs"=>[e1, e2, e3, e4])
-
-    modeldata["load_multipliers"] = (1:nincr)./nincr*1.0;
-    modeldata["maxdu_tol"] = maxdu_tol;
-    modeldata["maxiter"] = maxiter;
-    modeldata["line_search"]  = true;
-    modeldata["iteration_observer"] = (lambda, iter, du, modeldata) -> begin
-        @show lambda, iter, modeldata["maxdu"], modeldata["maxbal"]
-    end
-    Ux = FFlt[]; Rx = FFlt[]
-    modeldata["increment_observer"] = (lambda, modeldata) -> begin
-        push!(Ux, mean(modeldata["un1"].values[movel1,1]));
-        push!(Rx, sum(modeldata["reactions"].values[movel1,1]));
-    end
-
-    modeldata = nonlinearstatics(modeldata);
-    @show Ux, Rx
-
-    pl = lineplot(Ux, Rx)
-    display(pl)
+	delta = (I, J) -> I == J ? 1.0 : 0.0
+	lambda = 3.3
+	mu = 0.156
+	F = [1.02 0.03 -0.04; 0.01 0.99 -0.03; -0.01 0.02 0.95]
+	C = fill(0.0, 3, 3, 3, 3)
+	for I in 1:3, J in 1:3, K in 1:3, L in 1:3
+		C[I, J, K, L] = lambda * delta(I, J) * delta(K, L) + 
+		mu * (delta(I, K) * delta(J, L) + delta(I, L) * delta(J, K))
+	end
+	Cm = tens4_symm_to_6(C)
+	c = fill(0.0, 6, 6)
+	totalLagrangean2current!(c, Cm, F)
+	c2 = fill(0.0, 3, 3, 3, 3)
+	for i in 1:3, j in 1:3, k in 1:3, l in 1:3
+		c2[i, j, k, l] = 0.0
+		for I in 1:3, J in 1:3, K in 1:3, L in 1:3
+			c2[i, j, k, l] += C[I, J, K, L] / det(F) * F[i, I] * F[j, J] * F[k, K] * F[l, L]
+		end
+	end
+	c2m = tens4_symm_to_6(c2)
+	@test norm(c - c2m) <= 1.0e-12
 end
 end
-using .mneohook1
-mneohook1.test()
+using .testelasticitytensor1
+testelasticitytensor1.test()
