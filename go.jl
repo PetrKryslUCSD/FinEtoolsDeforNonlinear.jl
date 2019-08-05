d768c57d279605baaafd34a0be1edc90732bb143
Petr Krysl <pkrysl@ucsd.edu>
2019-08-05 20:22:15 +0200

    go file

--- go.jl
+++ go.jl
@@ -3,7 +3,11 @@ Pkg.activate(".")
 # Pkg.instantiate()
 using FinEtoolsDeforNonlinear
 
+# Pkg.test()
+
 # include(".\\examples\\statics\\2-d\\tension_compression_examples.jl");
 # tension_compression_examples.neohookeanad_q4()
 
-Pkg.test()
+cd(".\\examples\\statics\\2-d")   
+include(".\\bertoldi_compression_examples.jl"); 
+bertoldi_compression_examples.neohookeanad_q4()
