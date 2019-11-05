bfe100311979a3d2ac9a28e655268a9ef26006e6
Petr Krysl <pkrysl@ucsd.edu>
2019-11-05 21:02:26 +0100

    update go

--- go.jl
+++ go.jl
@@ -8,6 +8,6 @@ using FinEtoolsDeforNonlinear
 # include(".\\examples\\statics\\2-d\\tension_compression_examples.jl");
 # tension_compression_examples.neohookeanad_q4()
 
-cd(".\\examples\\statics\\2-d")   
-include(".\\bertoldi_compression_examples.jl"); 
-bertoldi_compression_examples.neohookeanad_q4()
+# cd(".\\examples\\statics\\2-d")   
+# include(".\\bertoldi_compression_examples.jl"); 
+# bertoldi_compression_examples.neohookeanad_q4()
