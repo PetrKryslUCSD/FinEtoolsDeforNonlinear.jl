c9df25936881cbc8f239432f3f0b07d445352749
Petr Krysl <pkrysl@ucsd.edu>
2019-11-10 21:03:02 +0100

    Get rid of ADC

--- go.jl
+++ go.jl
@@ -2,8 +2,8 @@ using Pkg
 Pkg.activate(".")
 # Pkg.instantiate()
 using FinEtoolsDeforNonlinear
-
-# Pkg.test()
+# include("test/playground.jl")
+Pkg.test()
 
 # include(".\\examples\\statics\\2-d\\tension_compression_examples.jl");
 # tension_compression_examples.neohookeanad_q4()
--- src/FinEtoolsDeforNonlinear.jl
+++ src/FinEtoolsDeforNonlinear.jl
@@ -9,11 +9,12 @@ module FinEtoolsDeforNonlinear
 __precompile__(true)
 
 include("MatDeforNonlinearModule.jl")
-include("MatDeforNeohookeanModule.jl")
-include("MatDeforNeohookeanADModule.jl")
-include("MatDeforStVKModule.jl")
-include("MatDeforStVKADModule.jl")
-include("MatDeforI1RivlinADModule.jl")
+include("MatDeforI1RivlinADModule.jl")                
+include("MatDeforMooneyRivlinADModule.jl")            
+include("MatDeforNeohookeanModule.jl")         
+include("MatDeforNeohookeanADModule.jl")                              
+include("MatDeforStVKADModule.jl")                    
+include("MatDeforStVKModule.jl")  
 
 include("FEMMDeforNonlinearBaseModule.jl")
 include("FEMMDeforNonlinearModule.jl")
