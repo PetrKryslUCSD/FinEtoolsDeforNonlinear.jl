c95cc60c391859c31cfaf94eafb4f5f809534f4c
Petr Krysl <pkrysl@ucsd.edu>
2019-11-11 18:00:27 +0100

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ include("test_materials.jl")
 include("test_materials2.jl") 
 include("test_materials3.jl") 
 include("test_materials4.jl") 
+include("test_materials5.jl") 
 end
 
 @time @testset "Operations" begin 
